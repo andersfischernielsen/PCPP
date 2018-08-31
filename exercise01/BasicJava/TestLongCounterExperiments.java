ab690e8d08dcf047c9779cd0ce6f6d6a0788207e
Anders Fischer-Nielsen <andersfischern@me.com>
2018-08-31 12:54:41 +0200

    Finished exercise 1.1, 1.2.

--- exercise01/BasicJava/TestLongCounterExperiments.java
+++ exercise01/BasicJava/TestLongCounterExperiments.java
@@ -3,31 +3,62 @@
 
 public class TestLongCounterExperiments {
   public static void main(String[] args) {
-    final LongCounter lc = new LongCounter();
     final int counts = 10_000_000;
     Thread t1 = new Thread(() -> {
       for (int i=0; i<counts; i++) 
-	lc.increment();
-    });
+        Printer.print();
+      });
+
     Thread t2 = new Thread(() -> {
       for (int i=0; i<counts; i++) 
-	lc.increment();
+      Printer.print();
     });
+    
     t1.start(); t2.start();
     try { t1.join(); t2.join(); }
     catch (InterruptedException exn) { 
       System.out.println("Some thread was interrupted");
     }
-    System.out.println("Count is " + lc.get() + " and should be " + 2*counts);
+  
+    //   final LongCounter lc = new LongCounter();
+  //   final int counts = 10_000_000;
+  //   Thread t1 = new Thread(() -> {
+  //     for (int i=0; i<counts; i++) 
+	// lc.decrement();
+  //   });
+  //   Thread t2 = new Thread(() -> {
+  //     for (int i=0; i<counts; i++) 
+	// lc.increment();
+  //   });
+  //   t1.start(); t2.start();
+  //   try { t1.join(); t2.join(); }
+  //   catch (InterruptedException exn) { 
+  //     System.out.println("Some thread was interrupted");
+  //   }
+  //   System.out.println("Count is " + lc.get() + " and should be " + 0);
   }
 }
 
 class LongCounter {
   private long count = 0;
   public synchronized void increment() {
-    count = count + 1;
+    count = count+=1;
   }
   public  long get() { 
     return count; 
   }
+  public synchronized long decrement() { 
+    return count-=1; 
+  }
+}
+
+class Printer {
+  
+  public static void print() {
+    synchronized (Printer.class) {
+      System.out.print("-");
+      try { Thread.sleep(50); } catch (InterruptedException exn) { }
+      System.out.print("|");
+    }
+  } 
 }

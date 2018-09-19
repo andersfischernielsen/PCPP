115ce97cf3f71d85963c1487ad61476a9dd645bf
Anders Fischer-Nielsen <andersfischern@me.com>
2018-09-19 12:47:21 +0200

    Made shit compile.

--- exercise03/Benchmarking/TestThreadTimingLatch.java
+++ exercise03/Benchmarking/TestThreadTimingLatch.java
@@ -97,13 +97,3 @@ class PrimeFactors {
     return p == prod;
   }
 }
-
-// Crude wall clock timing utility, measuring time in seconds
-   
-class Timer {
-  private long start, spent = 0;
-  public Timer() { play(); }
-  public double check() { return (System.nanoTime()-start+spent)/1e9; }
-  public void pause() { spent += System.nanoTime()-start; }
-  public void play() { start = System.nanoTime(); }
-}

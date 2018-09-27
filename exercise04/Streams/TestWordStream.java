86d310da443e68fc2e47a8d88bc2de4225f7e747
Anders Fischer-Nielsen <andersfischern@me.com>
2018-09-27 08:58:38 +0200

    Finished remaining exercises.

--- exercise04/Streams/TestWordStream.java
+++ exercise04/Streams/TestWordStream.java
@@ -19,7 +19,7 @@ public class TestWordStream {
   public static void main(String[] args) {
     String filename = "/usr/share/dict/words";
     Stream<String> words = readWords(filename);
-    System.out.println(countEs(filename));
+    System.out.println(countAnagrams(filename));
   }
 
   private static void printStream(Stream stream) {
@@ -84,23 +84,37 @@ public class TestWordStream {
   }
 
   //4.3.9
-  public static TreeMap<Character, Long> buildCharacterTree(String s) {
-    return s.chars()
+  public static TreeMap<Character, Long> letters(String s) {
+    return s.toLowerCase().chars()
       .mapToObj(c -> (char) c)
       .collect(Collectors.groupingBy(c -> c, TreeMap::new, Collectors.counting()));
   }
 
-
   //4.3.10
   public static long countEs(String filename) {
-    var counts = readWords(filename).reduce("", (w, acc) -> w + acc);
-    var es = buildCharacterTree(counts).get('e');
-    return es;
+    String allWords = readWords(filename).reduce("", (w, acc) -> w + acc);
+    return letters(allWords).get('e');
   }
 
-  public static Map<Character,Integer> letters(String s) {
-    Map<Character,Integer> res = new TreeMap<>();
-    // TO DO: Implement properly
-    return res;
+  //4.3.11
+  public static int countAnagrams(String filename) {
+    Stream<String> allWords = readWords(filename);
+    //There's a bug here. So, every word is converted to a TreeMap, and then grouped by the equalities of the TreeMap. 
+    //Or so we'd expect. But it seems that the equality check in groupingBy() doesn't "realize" that two TreeMaps are equal.
+    //We therefore get way too many results. We have tried forcin a comparison using TreeMap::equals without success.
+    //We even tried collecting on m -> m.toString, without success, since the trees are ordered according to the natural 
+    //ordering of keys (chars here), toString on the TreeMap should produce the same string for two identical maps, 
+    //but this still gives too many results. 
+    var groupedMaps = allWords.map(w -> letters(w)).collect(Collectors.groupingBy(m -> m));
+    //                                                                            ^^^^^^ should result in a.compareTo(b) for TreeMap a and TreeMap b, right? 
+    return groupedMaps.size();
+  }
+
+  //4.3.12 & 4.3.13
+  public static int countAnagramsParallel(String filename) {
+    //Slapping on parallel seems to make the execution slower. GroupingByConcurrent is a better variant.
+    Stream<String> allWords = readWords(filename);
+    var groupedMaps = allWords./*parallel().*/map(w -> letters(w)).collect(Collectors.groupingByConcurrent(m -> m));
+    return groupedMaps.size();
   }
 }
